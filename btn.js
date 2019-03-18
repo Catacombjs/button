8f4c58b03637756186b1956b4e64525b5e45ec80
Catacombjs <inthegotemcity@gmail.com>
2019-03-18 08:00:34 +0300

    first commit

---
+++ btn.js
@@ -0,0 +1,10 @@
+$( window ).load(function() {
+  $( "div" ).effect( "bounce", "slow" );
+});
+
+      $(window).ready(setInterval(function(){
+        
+  $( "div" ).effect( "bounce", 1000 );
+},3000));
+             
+
